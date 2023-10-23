0ebbd907b3c358998f7bd19db20b0315d7eff489
BenScherrer <ben.scherrer59@gmail.com>
2023-10-23 13:18:33 +0200

    adjusting FlaskAPI to execute train and predict

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
-# Flask API
-from flask import Flask
+# Public modules
+from flask import Flask, request
+import os
+import sys
+
+# Entry point
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_dir = os.path.dirname(current_dir)
+sys.path.append(project_dir)
+
+# Private modules
+from src.scripts.train import train
+from src.scripts.evaluate import predict
+from src.scripts.evaluate import evaluate
 
 app = Flask(__name__)
 
@@ -8,16 +20,23 @@ def hello_world():
     return '<p>Hello, World!</p>'
 
 
-@app.route('/train')
-def train():
+@app.route('/train', methods=['GET'])
+def api_train():
+    data_path = request.args.get('data_path')
+    method = request.args.get('method')
+    train(data_path, method)
     return 'Training...'
 
-@app.route('/predict')
-def predict():
-    return 'Predict is...'
+@app.route('/predict', methods=['GET'])
+def api_predict():
+    instance = request.args.get('instance')
+    model_folder = request.args.get('model_path')
+    method = request.args.get('method')
+    res = predict(model_folder, instance, method)
+    return f'Predict is {res}'
 
 @app.route('/evaluate')
-def evaluate():
+def api_evaluate():
     return 'Model evaluated'
 
 
